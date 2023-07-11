b12b9fe306771c9370a4adcf9c50bc80a38d896b
DIEGOHORVATTI <d.horvattid@gmail.com>
2023-07-11 03:34:19 -0300

    feat: component of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,43 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
+
+import Logo from '../assets/logo.svg'
+import { Iconify } from './Iconify'
 
 export const Header = () => {
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        Header
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: '#000000',
+        color: '#fff',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Grid item>
+        <img src={Logo} width={100} height={100} alt="Logo" />
+      </Grid>
+
+      <Grid item>
+        <Grid container spacing={0.5} alignItems="center">
+          <Grid item>
+            <Typography
+              variant="h4"
+              component="h1"
+              sx={{
+                fontFamily: 'UnifrakturMaguntia, sans-serif'
+              }}
+            >
+              Todo List
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <Iconify icon="basil:edit-solid" size={3} />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App.tsx'
+import { Grid } from '@mui/material'
+
+import { Header } from './components/Header.tsx'
+import { Home } from './pages/Home.tsx'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=UnifrakturMaguntia&display=swap"
+      rel="stylesheet"
+    />
+
+    <Grid container>
+      <Header />
+      <Home />
+    </Grid>
   </React.StrictMode>
 )
